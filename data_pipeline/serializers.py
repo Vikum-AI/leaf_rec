36061c99bc8f446c0dc354a3b5d595cbbcf95af0
Vikum Dabare <vikumdabare@gmail.com>
2023-04-04 22:58:31 +0530

    init

---
+++ data_pipeline/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import ProcessImage
+
+class ProcessImageSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return ProcessImage.create_with_current_datetime(
+            image_src=validated_data['image_src']
+        )
+
+    class Meta:
+        model = ProcessImage
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        
+        
+class FileUploadSerializer(serializers.ModelSerializer):
+    file = serializers.FileField(use_url=False)
+    
+    def update(self, validated_data):
+        file_obj = validated_data['file']
+        print('file_obj', validated_data)
+        return validated_data
+    
+    class Meta:
+        model = ProcessImage
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        
